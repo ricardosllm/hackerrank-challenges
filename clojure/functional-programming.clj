646f9d15f1556691dd5171ebadf44a7c81d9a3cf
Ricardo Mota <ricardosllm@gmail.com>
2016-02-04 21:24:15 +0800

    Jumping Bunnies

--- clojure/functional-programming.clj
+++ clojure/functional-programming.clj
@@ -8,3 +8,11 @@
     (dotimes [x (count lst)] 
         (dotimes [_ num] (println (+ x 1)))))
 
+; Jumping Bunnies
+(defn gcd [a b] (if (zero? b) a (recur b (mod a b))))  ; greatest common divisor
+(defn lcm [a b] (/ (* a b) (gcd a b)))                 ; lowest common multiple 
+
+(let [n-s (read-line)
+      j-s (read-line)
+      j (map read-string (split j-s #"\s+"))]
+      (println (reduce lcm j)))
