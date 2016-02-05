f0eeae49e4a5cb560f897bb642a7185eb2004c5b
Ricardo Mota <ricardosllm@gmail.com>
2016-02-05 11:18:35 +0800

    Rotate String

--- clojure/functional-programming.clj
+++ clojure/functional-programming.clj
@@ -1,14 +1,13 @@
-; Hello World N Times
-(fn [n] 
-    (dotimes [_ n] (println "Hello World"))
-
-
 ; List Replication
+
 (fn [num lst]
     (dotimes [x (count lst)] 
         (dotimes [_ num] (println (+ x 1)))))
 
+; ----------------------------------------------
+
 ; Jumping Bunnies
+
 (defn gcd [a b] (if (zero? b) a (recur b (mod a b))))  ; greatest common divisor
 (defn lcm [a b] (/ (* a b) (gcd a b)))                 ; lowest common multiple 
 
@@ -16,3 +15,26 @@
       j-s (read-line)
       j (map read-string (split j-s #"\s+"))]
       (println (reduce lcm j)))
+  
+; ----------------------------------------------
+
+; Rotate String
+
+(defn rotate-word [w]
+  (if (= 1 (count w))
+    w
+    (str (apply str (rest w)) (first w))))
+
+(let [t (Integer/parseInt (read-line))]
+  (dotimes [i t]
+    (let [word (read-line)
+          lngth (count word)]
+      (loop [i lngth
+             to-p word]
+        (let [next-word (rotate-word to-p)]
+          (print next-word)
+          (print " ")
+          (if (> i 1) (recur (- i 1) next-word))))
+      (println))))
+  
+  ; ----------------------------------------------
