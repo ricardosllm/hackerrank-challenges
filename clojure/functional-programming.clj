d4fd6d38fad84ca73175333e442013002cb86baa
Ricardo Mota <ricardosllm@gmail.com>
2016-02-14 18:40:31 +0800

    Sierpinski - WIP

--- clojure/functional-programming.clj
+++ clojure/functional-programming.clj
@@ -145,15 +145,15 @@
 ; Sierpinski triangles
 
 (defn sierpinkski [n]
-  (dotimes [y 31]
-    (dotimes [x 62]
-      (if (<= x 31)
+  (dotimes [y 32]
+    (dotimes [x 63]
+      (if (< x 32)
         ; "1" for y < x "_" for y > x
         (if (>= y (+ (- x) 31)) (print "1") (print "_"))
         ; "1" for y > x "_" for y < x
         (if (>= (+ 31 y) x) (print "1") (print "_"))
-        )
       )
+    )
     (println)))
 
 (let [n (Integer/parseInt (read-line))]
