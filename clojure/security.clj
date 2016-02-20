fc93c9a82ed3618431d7bd98cc2fa8b7b3d0eed5
Ricardo Mota <ricardosllm@gmail.com>
2016-02-20 12:06:35 +0800

    Security - Clojure

---
+++ clojure/security.clj
@@ -0,0 +1,12 @@
+; ----------------------------------------------
+; Security - functions
+
+(let [x (Integer/parseInt (read-line))]
+  (println (mod x 11)))
+
+; ----------------------------------------------
+; Security - function - ii
+
+(let [x (Integer/parseInt (read-line))]
+  (println (* x x)))
+
