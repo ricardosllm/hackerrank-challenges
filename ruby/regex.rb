664fb1f0cb74e44715662f973d9bc1ca19fdffc8
Ricardo Mota <ricardosllm@gmail.com>
2016-03-04 10:44:15 +0800

    Update regex.rb

--- ruby/regex.rb
+++ ruby/regex.rb
@@ -1,5 +1,5 @@
 #######################################
 
-Regex_Pattern = '.{3}\..{3}\..{3}\..{3}'    # Matching Anything But a Newline
+'.{3}\..{3}\..{3}\..{3}'    # Matching Anything But a Newline
 
-Regex_Pattern = '\d{2}\D{1}\d{2}\D{1}\d{4}' # Matching Digits & Non-Digit Characters
+'\d{2}\D{1}\d{2}\D{1}\d{4}' # Matching Digits & Non-Digit Characters
