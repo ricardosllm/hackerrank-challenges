a2242bbff93b8d7245e2114f7bad58733ad51cef
Ricardo Mota <ricardosllm@gmail.com>
2016-03-04 10:43:55 +0800

    regex - Digits & Non-Digit Characters

--- ruby/regex.rb
+++ ruby/regex.rb
@@ -1,6 +1,5 @@
 #######################################
-# Matching Anything But a Newline
 
-Regex_Pattern = '.{3}\..{3}\..{3}\..{3}'
+Regex_Pattern = '.{3}\..{3}\..{3}\..{3}'    # Matching Anything But a Newline
 
-#######################################
+Regex_Pattern = '\d{2}\D{1}\d{2}\D{1}\d{4}' # Matching Digits & Non-Digit Characters
