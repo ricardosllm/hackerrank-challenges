49f7e14231f0cecfe5886ed9f01cf2ed174c6d16
Ricardo Mota <ricardosllm@gmail.com>
2016-03-04 10:47:18 +0800

    regex - Whitespace & Non-Whitespace Character

--- ruby/regex.rb
+++ ruby/regex.rb
@@ -3,3 +3,5 @@
 '.{3}\..{3}\..{3}\..{3}'    # Matching Anything But a Newline
 
 '\d{2}\D{1}\d{2}\D{1}\d{4}' # Matching Digits & Non-Digit Characters
+
+'\S{2}\s{1}\S{2}\s{1}\S{2}' # Matching Whitespace & Non-Whitespace Character
