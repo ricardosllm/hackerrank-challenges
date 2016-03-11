ca29f37dfff14c200b7b34cc4cc7761a2db392a6
Ricardo Mota <ricardosllm@gmail.com>
2016-03-11 11:07:36 +0800

    Update strings.rb

--- ruby/strings.rb
+++ ruby/strings.rb
@@ -10,45 +10,46 @@ def double_quote
 end
 
 def here_doc
-	<<-HERE
+  <<-HERE
   Hello World and others!
   HERE
 end
 
 # Encoding
 def transcode(iso_encoded)
-	iso_encoded.force_encoding 'utf-8'
+  iso_encoded.force_encoding 'utf-8'
 end
 
 # Indexing
 def extract_number(string, pos, length)
-	num = string[pos,length]
-	num[2,1] = ""
-	num.to_i / 100.00
+  num = string[pos,length]
+  num[2,1] = ""
+  num.to_i / 100.00
 end
 
 def serial_average(serial)
-	x = extract_number serial, 4, 5
-	y = extract_number serial, 10, 5
-	average = ((x + y) / 2.00).round 2
-	"#{serial[0,4]}#{average}"
+  x = extract_number serial, 4, 5
+  y = extract_number serial, 10, 5
+  average = ((x + y) / 2.00).round 2
+  "#{serial[0,4]}#{average}"
 end
 
 # Iteration
 def count_multibyte_char(string)
-	string.each_char.with_object([]) { |c, r| r << c if c.bytesize > 1 }.size
+  string.each_char.with_object([]) { |c, r| r << c if c.bytesize > 1 }.size
 end
 
 # Methods I
 def process_text(array)
-	array.map(&:chomp).map(&:strip).join ' '
+  array.map(&:chomp).map(&:strip).join ' '
 end
 
 # Methods II
 def strike(string)
-	"<strike>#{string}</strike>"
+  "<strike>#{string}</strike>"
 end
 
 def mask_article(string, array)
-	string.split.each.map { |word| array.include?(word) ? strike(word) : word }.join ' '
+  string.split.each.map { |word| array.include?(word) ? strike(word) : word }.join ' '
 end
+
