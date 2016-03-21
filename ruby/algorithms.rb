550242f94ac01b8b66d12cccfc19157ac007b0e1
Ricardo Mota <ricardosllm@gmail.com>
2016-03-21 10:25:00 +0800

    Cut the Sticks

--- ruby/algorithms.rb
+++ ruby/algorithms.rb
@@ -11,3 +11,21 @@ tests = (0...test_cases).each.with_object([]) do |tst, tests|
 end
 
 tests.each { |enter, exit| puts width[enter..exit].min }
+
+#############################
+# Cut the Sticks
+
+def cut_the_sticks(sticks)
+  if sticks.size > 0 
+    p sticks.size
+    smallest = sticks.min
+    cut_the_sticks(
+      sticks
+        .map    { |stick| stick - smallest }
+        .select { |stick| stick > 0 }
+    )
+  end
+end
+
+n = gets.strip.to_i
+cut_the_sticks gets.strip.split(' ').map(&:to_i)
